8bfebff07051b8a514905d1fd6b69063d10d646c
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2023-02-04 15:29:54 +0530

    Add files via upload

---
+++ Roottoleafpaths.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Roottoleafpaths {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    public static void printpath(ArrayList<Integer> path){
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i)+"->");
+        }
+        System.out.println("Null");
+    }
+    public static void printroot2leaf(Node root,ArrayList<Integer> path){
+        if(root==null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left==null&&root.right==null){
+            printpath(path);
+        }
+        printroot2leaf(root.left,path);
+        printroot2leaf(root.right,path);
+        path.remove(path.size()-1);
+    }
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        printroot2leaf(root,new ArrayList<>());
+        
+    }
+}
